fb030dd10587adf96d574b3963c6008b2c8887d3
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2022-03-13 20:10:52 +0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+    application
+}
+
+val ktorVersion = "1.6.7"
+val logbackVersion = "1.2.5"
+
+group = "me.charles.maina"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation( "io.ktor:ktor-server-netty:$ktorVersion")
+    implementation ("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation ("io.ktor:ktor-serialization:$ktorVersion")
+
+    testImplementation ("io.ktor:ktor-server-test-host:$ktorVersion")
+    testImplementation ("org.jetbrains.kotlin:kotlin-test")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
