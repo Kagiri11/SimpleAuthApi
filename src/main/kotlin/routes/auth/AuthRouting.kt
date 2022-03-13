a825dda4e8b066bdee01a017c2efd06ece6e0b29
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2022-03-13 21:49:22 +0300

    Created application users collection

--- src/main/kotlin/routes/auth/AuthRouting.kt
+++ src/main/kotlin/routes/auth/AuthRouting.kt
@@ -8,21 +8,35 @@ import io.ktor.routing.*
 import models.auth.UserCredentials
 import models.user.User
 import org.litote.kmongo.coroutine.CoroutineCollection
+import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.coroutine.insertOne
+import org.litote.kmongo.reactivestreams.KMongo
 
 fun Route.register(usersCollection: CoroutineCollection<User>){
-    post("/register"){
-        val user = call.receive<UserCredentials>()
-        val username = user.username
-        val password = user.password
-        val users = usersCollection.find().toList()
-        if (users.find { it.username == username && it.password == password } != null){
-            usersCollection.insertOne(User(username = username, password = password))
-            call.respondText("You have registered successfully", status = HttpStatusCode.Created)
-        }else{
-            call.respondText("User already exists", status = HttpStatusCode.Conflict)
+
+}
+
+object Database{
+    private val client = KMongo.createClient().coroutine
+    private val database = client.getDatabase("my_Database")
+    val usersCollection = database.getCollection<User>()
+}
+
+fun Application.authRouting(){
+
+    routing {
+        post("/register"){
+            val user = call.receive<UserCredentials>()
+            val username = user.username
+            val password = user.password
+            val users = Database.usersCollection.find().toList()
+            if (users.find { it.username == username && it.password == password } != null){
+                Database.usersCollection.insertOne(User(username = username, password = password))
+                call.respondText("You have registered successfully", status = HttpStatusCode.Created)
+            }else{
+                call.respondText("User already exists", status = HttpStatusCode.Conflict)
+            }
         }
-       
     }
 }
 
