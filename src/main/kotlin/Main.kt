10156fb59a7252710f02a56be3ad356e6c9cb36c
Kagiri Charles <59829833+Kagiri11@users.noreply.github.com>
2022-03-15 23:25:52 +0300

    Moved Database to utils

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,10 +3,17 @@ import io.ktor.features.*
 import io.ktor.gson.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import kotlinx.coroutines.runBlocking
+import models.user.User
 import routes.auth.authRouting
+import utils.Database
 
 fun main() {
+    val userOne = User(username = "First User", password = "password")
    embeddedServer(Netty, port = 8080){
+       runBlocking {
+           Database.usersCollection.insertOne(userOne)
+       }
         module()
    }.start(wait = true)
 }
--- src/main/kotlin/routes/auth/AuthRouting.kt
+++ src/main/kotlin/routes/auth/AuthRouting.kt
@@ -7,14 +7,7 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import models.auth.UserCredentials
 import models.user.User
-import org.litote.kmongo.coroutine.coroutine
-import org.litote.kmongo.reactivestreams.KMongo
-
-object Database {
-    private val client = KMongo.createClient().coroutine
-    private val database = client.getDatabase("my_Database")
-    val usersCollection = database.getCollection<User>()
-}
+import utils.Database
 
 fun Application.authRouting() {
     routing {
@@ -23,7 +16,7 @@ fun Application.authRouting() {
             val username = user.username
             val password = user.password
             val users = Database.usersCollection.find().toList()
-            if (users.find { it.username == username && it.password == password } != null) {
+            if (users.find { it.username == username && it.password == password } == null) {
                 Database.usersCollection.insertOne(User(username = username, password = password))
                 call.respondText("You have registered successfully", status = HttpStatusCode.Created)
             } else {
@@ -31,7 +24,7 @@ fun Application.authRouting() {
             }
         }
 
-        get("/users"){
+        get("/users") {
             call.respond(Database.usersCollection.find().toList())
         }
     }
